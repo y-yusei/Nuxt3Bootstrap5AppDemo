230fe20754cd9e5824126d7b670b0c9128b5b041
y-yusei <146802150+y-yusei@users.noreply.github.com>
2023-12-05 07:30:38 +0000

    henkou

--- pages/index.vue
+++ pages/index.vue
@@ -1,54 +1,65 @@
 <template>
-<div class="container py-5 mt-5">
-<main class="px-3">
-    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-    <h1>この観光名所はどこ？</h1>
-    <h1>都道府県当てゲーム！</h1>
-    <h2>全国の観光名所の写真が表示され、その写真はどの都道府県なのかを当てるゲームです。</h2>
-  </div>
-</main>
-  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm">
-        <div class="card-header py-3 text-white bg-success">
-          <h4 class="my-0 fw-normal display-4">Easy</h4>
-        </div>
-        <div class="card-body">
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>説明文</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-outline-success">Game Strat</button>
+  <div class="container py-5 mt-5">
+    <main class="px-3">
+      <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">この観光名所はどこ？</h1>
+        <p class="text-decoration-underline fw-bold display-1">都道府県当てゲーム！</p>
+        <p class="fs-3">全国の観光名所の写真が表示され、</p>
+        <p class="fs-3">その写真はどの都道府県なのかを当てるゲームです。</p>
+      </div>
+    </main>
+
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3 text-white bg-success">
+            <h4 class="my-0 fw-normal display-4">Easy</h4>
+          </div>
+          <div class="card-body">
+            <p class="mt-3 mb-4">簡単なレベルの説明文が入ります。</p>
+            <button type="button" class="w-100 btn btn-lg btn-outline-success">スタート</button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm">
-        <div class="card-header py-3 text-white bg-warning">
-          <h4 class="my-0 fw-normal display-4">Normal</h4>
-        </div>
-        <div class="card-body">
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>説明文</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-outline-warning">Game Strat</button>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3 text-white bg-warning">
+            <h4 class="my-0 fw-normal display-4">Normal</h4>
+          </div>
+          <div class="card-body">
+            <p class="mt-3 mb-4">普通のレベルの説明文が入ります。</p>
+            <button type="button" class="w-100 btn btn-lg btn-outline-warning">スタート</button>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm border-danger">
-        <div class="card-header py-3 text-white bg-danger">
-          <h4 class="my-0 fw-normal display-4">Hard</h4>
-        </div>
-        <div class="card-body">
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>説明文</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-outline-danger">Game Strat</button>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-danger">
+          <div class="card-header py-3 text-white bg-danger">
+            <h4 class="my-0 fw-normal display-4">Hard</h4>
+          </div>
+          <div class="card-body">
+            <p class="mt-3 mb-4">難しいレベルの説明文が入ります。</p>
+            <button type="button" class="w-100 btn btn-lg btn-outline-danger">スタート</button>
           </div>
         </div>
       </div>
     </div>
   </div>
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://www.ana.co.jp/japan-travel-planner/area/kinki/hyogo/0000004/img005.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://s3.ap-northeast-1.amazonaws.com/bukkenmatome.nxin.jp/life/images/20211224021152/th_th_pixta_77370244_M-2000x1333.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://www.keihanhotels-resorts.co.jp/the-thousand-kyoto/sight/asset/b5bcabab0fcde4e3b4969d388a4923f30fc7e549.png" alt="Third slide">
+    </div>
+  </div>
+</div>
+
 </template>
