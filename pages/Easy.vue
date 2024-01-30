7bd3e87242a51e29c47299f9d4dcfd96ecbd3399
y-yusei <146802150+y-yusei@users.noreply.github.com>
2024-01-30 07:32:50 +0000

    更新

--- pages/Easy.vue
+++ pages/Easy.vue
@@ -1,28 +1,49 @@
 <template>
+  <div class="all">
   <div class="title">
-    <h1 class="Start">Start</h1>
+    <h1 class="Start">Start</h1> 
   </div>
-  <div class="Game-Content">
-  </div>
+  <div class="quiz"><img src="main1.jpg"></div>
+</div>
 </template>
 
 <style scoped>
+.all {
+  position: relative;
+  max-width: 100vw;
+}
 .title {
-  width: 100vw;
-  height: 100vh;
+  position: relative;
+  width: 100%;
+  height: 91vh;
   display: flex;
   justify-content: center;
   align-items: center;
   background: #008000;
   animation: page 4s 0s ease-in-out forwards;
+  z-index: 20;
 }
 .Start {
+  position: relative;
   font-size: 250px;
   font-weight: bold;
   text-align: center;
   color: #fff;
   animation: fadeIn 3s 0s ease-in-out forwards;
+  z-index: 30;
 }
+.quiz {
+  display: inline-block;
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  margin-bottom: 0;
+  z-index: 10;
+}
+
 @keyframes page {
   0%,75%{
     opacity: 1;
@@ -41,5 +62,12 @@
     opacity: 1;
   }
 }
-
+@keyframes quiz {
+  0%,50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
