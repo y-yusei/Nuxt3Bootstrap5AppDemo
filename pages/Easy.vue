0cde2feb2414dd122b50211e1c7ee92668d4b207
y-yusei <146802150+y-yusei@users.noreply.github.com>
2024-02-02 04:53:49 +0000

    変更

--- pages/Easy.vue
+++ pages/Easy.vue
@@ -3,7 +3,24 @@
   <div class="title">
     <h1 class="Start">Start</h1> 
   </div>
-  <div class="quiz"><img src="main1.jpg"></div>
+  
+  <div class="quiz">
+  <div class="col-md-8 offset-md-2">
+    <div class="questionBox">
+      <div class="box-title">問題</div>
+      <p>この写真の都道府県はどこ？</p>
+    </div>
+  </div>
+
+  <img src="hyougo.jpg" class="img-thumbnail">
+
+  <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+  </div>
 </div>
 </template>
 
@@ -35,7 +52,7 @@
 .quiz {
   display: inline-block;
   position: absolute;
-  top: 30%;
+  top: 40%;
   left: 50%;
   transform: translate(-50%, -50%);
   -webkit-transform: translate(-50%, -50%);
@@ -62,12 +79,29 @@
     opacity: 1;
   }
 }
-@keyframes quiz {
-  0%,50% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
+.questionBox {
+    position: relative;
+    margin: 2em 0;
+    padding: 0.5em 1em;
+    border: solid 3px #049DBF;
+    border-radius: 8px;
+}
+
+.questionBox .box-title {
+    position: absolute;
+    display: inline-block;
+    top: -13px;
+    left: 10px;
+    padding: 0 9px;
+    line-height: 1;
+    font-size: 20px;
+    background: #FFF;
+    color: #049DBF;
+    font-weight: bold;
+}
+.questionBox p {
+    margin: 0;
+    padding: 5px;
+    font-size: 18px;
 }
 </style>
